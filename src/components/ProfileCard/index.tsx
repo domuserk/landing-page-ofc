4efeb9c260cbe075b533a16cd38597feba781f10
domuserk <rgdesenhos321@gmail.com>
2020-10-19 10:17:59 -0300

    Graphql

---
+++ src/components/ProfileCard/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { GrGithub } from 'react-icons/gr'
+import { FaDribbble, FaTwitter } from 'react-icons/fa'
+
+import * as S from './styles'
+import { Author } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
+
+const icons = {
+  Twitter: <FaTwitter />,
+  Github: <GrGithub />,
+  Dribbble: <FaDribbble />
+}
+
+const ProfileCard: React.FC<Author> = ({ name, role, photo, description }) => (
+  <S.Card key={name}>
+    <S.Image src={getImageUrl(photo.url)} alt={photo.alternativeText} />
+    <S.Name>{name}</S.Name>
+    <S.Role>{role}</S.Role>
+    <S.Description>{description}</S.Description>
+  </S.Card>
+)
+
+export default ProfileCard
